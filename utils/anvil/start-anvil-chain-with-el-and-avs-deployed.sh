2319070d88d2d8b89ef8ee7e51d73cad382812f3
Purva-Chaudhari <purva.chaudhari02@gmail.com>
2024-07-17 09:45:55 +0000

    fix deploy bash script

--- utils/anvil/start-anvil-chain-with-el-and-avs-deployed.sh
+++ utils/anvil/start-anvil-chain-with-el-and-avs-deployed.sh
@@ -2,6 +2,23 @@
 
 RPC_URL=http://localhost:8545
 
+# Ensure PATH includes the directory where cast is installed
+export PATH="$HOME/.foundry/bin:$PATH"
+
+# Source profile files to ensure environment variables are loaded
+if [ -f "$HOME/.profile" ]; then
+    source "$HOME/.profile"
+fi
+if [ -f "$HOME/.bashrc" ]; then
+    source "$HOME/.bashrc"
+fi
+
+# Verify cast command availability
+echo "Verifying cast command availability..."
+echo "Current PATH: $PATH"
+which cast
+cast --version
+
 # cd to the directory of this script so that this can be run from anywhere
 parent_path=$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -24,19 +41,18 @@ cleanup() {
 }
 trap 'cleanup $LINENO "$BASH_COMMAND"' EXIT
 
-# start an anvil instance in the background that has eigenlayer contracts deployed
-# we start anvil in the background so that we can run the below script
+# Start an anvil instance in the background that has eigenlayer contracts deployed
+# We start anvil in the background so that we can run the below script
 # anvil --load-state avs-and-eigenlayer-deployed-anvil-state.json &
 # FIXME: bug in latest foundry version, so we use this pinned version instead of latest
 start_anvil_docker $parent_path/avs-and-eigenlayer-deployed-anvil-state.json ""
 
 cd ../../contracts
-# we need to restart the anvil chain at the correct block, otherwise the indexRegistry has a quorumUpdate at the block number
-# at which it was deployed (aka quorum was created/updated), but when we start anvil by loading state file it starts at block number 0
-# so calling getOperatorListAtBlockNumber reverts because it thinks there are no quorums registered at block 0
-# advancing chain manually like this is a current hack until https://github.com/foundry-rs/foundry/issues/6679 is merged
+
+# Advancing the chain manually
+echo "Advancing the chain..."
 cast rpc anvil_mine 100 --rpc-url $RPC_URL
-echo "advancing chain... current block-number:" $(cast block-number)
+echo "Advancing chain... current block-number:" $(cast block-number)
 
 # Bring Anvil back to the foreground
 docker attach anvil
